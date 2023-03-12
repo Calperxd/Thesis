56b0157f86815629e284ff58b4ae4e8fc4e64708
Gabriel Calper Seabra <gabriel.seabra@itriad.org.br>
2023-03-12 15:10:12 -0400

    feat: startup_code with the functions to start RTOS

--- main.c
+++ main.c
@@ -1,32 +1,37 @@
 #include "setup.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-int main()
+// toggle the led 1 every second
+void taskLed1(void *pvParameters)
+{
+    while (1)
+    {
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+// toggle the led 2 every 200 second
+void taskLed2(void *pvParameters)
+{
+    while (1)
+    {
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+int main(void)
 {
     setupClock();
     setupLed();
-    while (1)
-    {
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 1);
-        for (size_t i = 0; i < 1000000; i++)
-        {
-            /* code */
-        }
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
-        for (size_t i = 0; i < 1000000; i++)
-        {
-            /* code */
-        }
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 2);
-        for (size_t i = 0; i < 1000000; i++)
-        {
-            /* code */
-        }
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0);
-        for (size_t i = 0; i < 1000000; i++)
-        {
-            /* code */
-        }
-    }
-    
-    return 0;
+    xTaskCreate((TaskFunction_t)taskLed1, "Led1", configMINIMAL_STACK_SIZE, NULL, 5, NULL );
+    xTaskCreate((TaskFunction_t)taskLed2, "Led2", configMINIMAL_STACK_SIZE, NULL, 5, NULL );
+    vTaskStartScheduler();
+    while (1){}
 }
--- source/startup_gcc.c
+++ source/startup_gcc.c
@@ -50,6 +50,9 @@ void ResetISR(void);
 static void NmiSR(void);
 static void FaultISR(void);
 static void IntDefaultHandler(void);
+extern void xPortPendSVHandler(void);
+extern void vPortSVCHandler(void);
+extern void xPortSysTickHandler(void);
 
 //*****************************************************************************
 //
@@ -86,11 +89,11 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     0,                                      // Reserved
     0,                                      // Reserved
-    IntDefaultHandler,                      // SVCall handler
+    vPortSVCHandler,                      // SVCall handler
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
-    IntDefaultHandler,                      // The PendSV handler
-    IntDefaultHandler,                      // The SysTick handler
+    xPortPendSVHandler,                      // The PendSV handler
+    xPortSysTickHandler,                      // The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
